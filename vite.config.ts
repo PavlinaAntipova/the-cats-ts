a3807a7be40a11d7d354e6ce9a1a9c4935b5f969
Pavlina Antipova <pavlina.antipova@gmail.com>
2023-05-24 18:15:40 +0200

    fix/favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 
 export default defineConfig({
-  plugins: [react(), vitePluginFaviconsInject('./src/assets/logo.svg')],
+  plugins: [react()],
   
 })
